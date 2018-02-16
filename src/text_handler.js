bf1f62c4ad762cf107c08f0fb7fa90d5a400796e
UladKasach <nappa300@gmail.com>
2018-02-15 23:14:10 -0500

    fix bug and reorganize dir structure

--- src/text_handler.js
+++ src/text_handler.js
@@ -1,6 +1,159 @@
 
 
 
+
+var global_textHandler_enforceHandler = {
+
+    alpha_only : function(textHandler){
+        var value = textHandler.value;
+        ///////////////////////////
+        var enforcedValue = value.replace(/[^a-zA-Z]+/,'');
+        //console.log(enforcedValue);
+
+        ///////////////////////////
+        if(value == enforcedValue){
+            return true;
+        }
+        return enforcedValue;
+    },
+    alpha_only_blur : function(){ return true; },
+
+    no_space : function(textHandler){
+        var value = textHandler.value;
+        ///////////////////////////
+
+        var enforcedValue = value.replace(/ /g,'');
+
+        ///////////////////////////
+        if(value == enforcedValue){
+            return true;
+        }
+        return enforcedValue;
+    },
+    no_space_blur : function(){ return true; },
+
+    digits_only : function(textHandler){
+        var value = textHandler.value;
+        ///////////////////////////
+
+        var enforcedValue = value.replace(/\D+/g, '');
+
+        ///////////////////////////
+        if(value == enforcedValue){
+            return true;
+        }
+        return enforcedValue;
+    },
+    digits_only_blur : function() {return true},
+
+    numeric_only : function(textHandler){
+        var value = textHandler.value;
+        ///////////////////////////
+
+        var enforcedValue = value.replace(/[^0-9.]/g, '');
+
+        ///////////////////////////
+        if(value == enforcedValue){
+            return true;
+        }
+        return enforcedValue;
+    },
+    numeric_only_blur : function() {return true},
+
+    address_only : function(){return true;},
+    address_only_blur : function(textHandler){
+        var value = textHandler.value;
+        var autocomplete_manager = textHandler.google_autocomplete_manager;
+        if(autocomplete_manager.enforce_input_is_valid_autocompleted_place() === false){
+            return false;
+        } else {
+            return true;
+        }
+
+        /*
+        var autocomplete_manager = textHandler.google_autocomplete_manager;
+        var autocomplete = autocomplete_manager.autocomplete_object;
+        //var place = autocomplete.getPlace();
+
+        //console.log(place);
+        //console.log(value);
+        if(value == "") return false;
+
+        var newAutocompleteService = new google.maps.places.AutocompleteService();
+        newAutocompleteService.getPlacePredictions(
+            {
+                'input': value,
+            },
+            function listentoresult(list, status) {
+                console.log(list[0]);
+                textHandler.DOM.inputElement.value = list[0].description; // update address in original text box
+                //console.log("Here i am");
+                //console.log(autocomplete);
+                //google.maps.event.trigger(autocomplete, 'place_changed'); // ensure original autocomplete is updated in response to text update
+                autocomplete_manager.place_id = list[0].place_id;
+            }
+        );
+        */
+
+
+
+
+    },
+
+
+
+    price : function(textHandler){
+        var value = textHandler.value;
+        var origValue = value;
+        ///////////////////////////
+        if(value == ""){
+            return true;
+        }
+        value = value.replace(/([^0-9\.])+/g,'');
+        var datas = value.split(".");
+
+        var valueA = datas[0];
+        var valueB = "";
+        var total = datas.length;
+        for(var index = 1; index < total; index++){
+            valueB += datas[index];
+        }
+        valueB = valueB.substr(0,2);
+        value = valueA;
+        if(datas.length > 1){
+            value += "." + valueB;
+        }
+
+        var enforcedValue = value;
+        value = origValue;
+        ///////////////////////////
+        if(value == enforcedValue){
+            return true;
+        }
+        return enforcedValue;
+    },
+    price_blur : function(textHandler){
+        var value = textHandler.value;
+        ///////////////////////////
+
+        price = value+"";
+        price = price.replace(/^0+/, '');
+        price = price.split(".");
+        if(price[0] == "") price[0] = "0";
+        if(price[1] == undefined) price[1] = "00";
+        while(price[1].length < 2) price[1] += "0";
+        price = price.join(".");
+
+        var enforcedValue = price;
+        ///////////////////////////
+        if(value == enforcedValue){
+            return true;
+        }
+        return enforcedValue;
+    },
+}
+
+
 function global_textHandler (){
     ////////////////////
     // Constants defined externally
@@ -251,159 +404,6 @@ global_textHandler.prototype.classes = {
         invalid : "text_handler_SupportTextColorDefault",
     },
 };
-
-
-
-var global_textHandler_enforceHandler = {
-
-    alpha_only : function(textHandler){
-        var value = textHandler.value;
-        ///////////////////////////
-        var enforcedValue = value.replace(/[^a-zA-Z]+/,'');
-        //console.log(enforcedValue);
-
-        ///////////////////////////
-        if(value == enforcedValue){
-            return true;
-        }
-        return enforcedValue;
-    },
-    alpha_only_blur : function(){ return true; },
-
-    no_space : function(textHandler){
-        var value = textHandler.value;
-        ///////////////////////////
-
-        var enforcedValue = value.replace(/ /g,'');
-
-        ///////////////////////////
-        if(value == enforcedValue){
-            return true;
-        }
-        return enforcedValue;
-    },
-    no_space_blur : function(){ return true; },
-
-    digits_only : function(textHandler){
-        var value = textHandler.value;
-        ///////////////////////////
-
-        var enforcedValue = value.replace(/\D+/g, '');
-
-        ///////////////////////////
-        if(value == enforcedValue){
-            return true;
-        }
-        return enforcedValue;
-    },
-    digits_only_blur : function() {return true},
-
-    numeric_only : function(textHandler){
-        var value = textHandler.value;
-        ///////////////////////////
-
-        var enforcedValue = value.replace(/[^0-9.]/g, '');
-
-        ///////////////////////////
-        if(value == enforcedValue){
-            return true;
-        }
-        return enforcedValue;
-    },
-    numeric_only_blur : function() {return true},
-
-    address_only : function(){return true;},
-    address_only_blur : function(textHandler){
-        var value = textHandler.value;
-        var autocomplete_manager = textHandler.google_autocomplete_manager;
-        if(autocomplete_manager.enforce_input_is_valid_autocompleted_place() === false){
-            return false;
-        } else {
-            return true;
-        }
-
-        /*
-        var autocomplete_manager = textHandler.google_autocomplete_manager;
-        var autocomplete = autocomplete_manager.autocomplete_object;
-        //var place = autocomplete.getPlace();
-
-        //console.log(place);
-        //console.log(value);
-        if(value == "") return false;
-
-        var newAutocompleteService = new google.maps.places.AutocompleteService();
-        newAutocompleteService.getPlacePredictions(
-            {
-                'input': value,
-            },
-            function listentoresult(list, status) {
-                console.log(list[0]);
-                textHandler.DOM.inputElement.value = list[0].description; // update address in original text box
-                //console.log("Here i am");
-                //console.log(autocomplete);
-                //google.maps.event.trigger(autocomplete, 'place_changed'); // ensure original autocomplete is updated in response to text update
-                autocomplete_manager.place_id = list[0].place_id;
-            }
-        );
-        */
-
-
-
-
-    },
-
-
-
-    price : function(textHandler){
-        var value = textHandler.value;
-        var origValue = value;
-        ///////////////////////////
-        if(value == ""){
-            return true;
-        }
-        value = value.replace(/([^0-9\.])+/g,'');
-        var datas = value.split(".");
-
-        var valueA = datas[0];
-        var valueB = "";
-        var total = datas.length;
-        for(var index = 1; index < total; index++){
-            valueB += datas[index];
-        }
-        valueB = valueB.substr(0,2);
-        value = valueA;
-        if(datas.length > 1){
-            value += "." + valueB;
-        }
-
-        var enforcedValue = value;
-        value = origValue;
-        ///////////////////////////
-        if(value == enforcedValue){
-            return true;
-        }
-        return enforcedValue;
-    },
-    price_blur : function(textHandler){
-        var value = textHandler.value;
-        ///////////////////////////
-
-        price = value+"";
-        price = price.replace(/^0+/, '');
-        price = price.split(".");
-        if(price[0] == "") price[0] = "0";
-        if(price[1] == undefined) price[1] = "00";
-        while(price[1].length < 2) price[1] += "0";
-        price = price.join(".");
-
-        var enforcedValue = price;
-        ///////////////////////////
-        if(value == enforcedValue){
-            return true;
-        }
-        return enforcedValue;
-    },
-}
 
 
 module.exports = global_textHandler;
